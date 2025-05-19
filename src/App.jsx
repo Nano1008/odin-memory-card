2e300dd7c1d6727d4bcd91b7ba7ac5f927ad9f29
Sinan <nanossn1008@gmail.com>
2025-05-19 19:33:50 +0100

    Fetch and display Pokémon data from PokéAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
-  return <h1>Hello!</h1>;
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      const response = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=12"
+      );
+      const data = await Promise.all(
+        response.data.results.map(async (pokemon) => {
+          const pokemonResponse = await axios.get(pokemon.url);
+          return {
+            name: pokemon.name,
+            image: pokemonResponse.data.sprites.front_default,
+            id: pokemonResponse.data.id,
+          };
+        })
+      );
+      setCards(shuffleCards(data));
+    };
+
+    fetchCards();
+  }, []);
+
+  const shuffleCards = (cards) => {
+    return [...cards].sort(() => Math.random() - 0.5);
+  };
+
+  return (
+    <div className="App">
+      <h1>Memory Card Game</h1>
+    </div>
+  );
 }
 
 export default App;
